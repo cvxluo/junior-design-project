1c3ad6cf8fbb2e360ed5cc8a3030d73b7cc0c61d
cvxluo <cvxluo@gmail.com>
2022-10-02 12:34:29 -0400

    Added MongoDB adapater to Next Auth in preparation for email provider

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,26 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "utils/mongodb";
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      httpOptions: {
+        timeout: 40000,
+      },
     }),
     // ...add more providers here
   ],
 };
-export default NextAuth(authOptions);
+export default NextAuth({
+  ...authOptions,
+  adapter: MongoDBAdapter(clientPromise),
+  debug: true,
+  session: {
+    strategy: "jwt",
+  },
+});
