ef04d2b753082af03f0da3f2b6aabe751d974abe
Alexa O'Reilly <aoreilly@gatech.edu>
2022-11-20 18:54:18 -0500

    added infoboxes

--- src/screens/Guidelines/Guidelines.jsx
+++ src/screens/Guidelines/Guidelines.jsx
@@ -23,6 +23,106 @@ const infoExample = {
   paragraphContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
 };
 
+const infoDosDonts = {
+  title: "II. Do's and Don'ts",
+  paragraphContent: "Do:",
+  listContent: [
+    "Be honest on the report",
+    "Take your time",
+    "Use action-oriented, hard-hitting impact statements",
+    "Get to the point",
+    "Quantify results",
+    "Fill up the entire form",
+    "Stay within your scope",
+    "Use bullet format",
+    "Spell out uncommon acronyms the first time you use them",
+    "Abbreviate words like gov't for goverment in the interest of space",
+    "Be error free (no misspellings, no extra spaces, correct capitalization",
+    "Review previous reports, PIF, and UIF if applicable",
+  ],
+  paragraphContent: "Don't:",
+  listContent: [
+    "Use prohibited statements",
+    "Include additional duties unless focused toward the mission",
+    "Use terminology only your organization understands",
+    "Leave white space, unless, of course, you're trying to send a negative message",
+    "Put your credibility on the line (don't rate everyone #1 if they aren't)",
+    "Use 'all caps' for emphasis",
+    "Use periods after bullets",
+    "Use more than one exclamation in a row",
+    "Use 'fluffy' statements with no meat behind them",
+  ],
+};
+
+const infoWordChoice = {
+  title: "III. Word Choice",
+  paragraphContent: "Use vivid words to emphasize action and results. Examples of vivid words:",
+  listContent: [
+    "Cut",
+    "Saved",
+    "Created",
+    "Drove",
+    "Guaranteed",
+    "Infuseed",
+    "Led",
+    "Spearheaded",
+    "Developed",
+    "Deftly",
+    "Diplomatically",
+    "Energetic",
+    "Rapidly",
+    "Persuasive",
+    "Dynamic",
+    "Instituted",
+  ],
+  paragraphContent: "Examples of mundane words:",
+  listContent: [
+    "Capable",
+    "Dependable",
+    "Effective",
+    "Coordinated",
+    "Sturdy",
+    "Monitored",
+    "Assisted",
+    "Aided",
+    "Contributed",
+    "Employed",
+    "Ready",
+    "Pontential",
+    "Participated",
+    "Reviewed",
+    "Adequate",
+    "Fine",
+    "Maintained",
+  ],
+};
+
+const infoBasics = {
+  title: "I. The Basics",
+  listContent: [
+    "Review the ratee's previous performance reports",
+    "Gather input from the ratee in advance",
+    "Don't use prohibited statements (see section xx)",
+    "Take the time to write a quality report",
+    "Proofread your product",
+    "Follow the do's and don'ts",
+    "Use strong word choice",
+    "Provide context",
+    "Stratify your descriptions",
+    "Use common acronyms instead of writing them out",
+  ],
+};
+
+const infoAcronyms = {
+  title: "IV. Acronyms",
+  paragraphContent: "The following are examples of some common acronyms that do not need to be written out:",
+  listContent: [
+    "ACC",
+    "ACSC",
+    "AEF",
+  ],
+};
+
 export default function Guidelines() {
   return (
     <div>
